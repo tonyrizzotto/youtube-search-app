3c4c9f33a9aa2e7a424bdb4f32886c2bfc2ce122
Tony Rizzotto <anthonyrizzotto@gmail.com>
2021-07-08 10:47:21 -0400

    Add: Basic search bar component
    Included some basic form styling with Semantic UI and basic JS to initialize a controlled state on the form.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    // prevent the default action on submit
+    event.preventDefault();
+  };
+
   render() {
-    return <div>Search Bar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
